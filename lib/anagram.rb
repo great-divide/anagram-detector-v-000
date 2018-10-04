e414ce3814207e62856651008cdedf0433d6a5ce
Brian Birdwell <bbirdwell@gmail.com>
2018-10-04 19:38:10 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    matches = []
+    if array.include?(@word)
+      matches << @word
+  end
+  
+  
+  
+end
